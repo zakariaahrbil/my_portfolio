38d79c40a9dd5f06dea8244d1598a92b8de8708f
zakaria ahrbil <zakariaahr@gmail.com>
2025-08-09 21:09:04 +0100

    refactor: clean up unused code and comments in Home component

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,6 @@ export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-
     const checkMobile = () => {
       setIsMobile(window.innerWidth < 768);
     };
@@ -54,8 +53,6 @@ export default function Home() {
         window.removeEventListener("resize", checkMobile);
         lenis.destroy();
       };
-    
-    
     }
 
     return () => {
@@ -65,11 +62,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-start justify-start min-h-screen bg-black w-full relative">
-      {/* <img
-        src={images.grain}
-        className="absolute inset-0 object-cover w-full h-full z-80 mix-blend-overlay opacity-70 pointer-events-none"
-      />
-      <Menu /> */}
+      <Menu />
 
       <main className="w-full relative overflow-hidden">
         <Hero />
